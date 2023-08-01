2f9108d5b27f6dce608438eef343fc44c4e59df5
farhadbaghban <baghbanfarhad@gmail.com>
2023-08-01 16:40:13 +0330

    added readme file

---
+++ Readme.txt
@@ -0,0 +1,7 @@
+first you have to install ((pysrt))
+
+pip install pysrt
+
+and then run 
+
+good luck
--- subtotxt.py
+++ subtotxt.py
@@ -1,14 +1,15 @@
-#getting text from srt files and create txt file
-import os, os.path,glob
+# getting text from srt files and create txt file
+import os, os.path, glob
 from pathlib import Path
 import pysrt
-readdirectory = "./subsrt" #srt file directory
-writedirectory = "./subtxt/" # text file directory
-for filename in glob.glob(os.path.join(readdirectory,"*.srt")):
+
+readdirectory = "./subsrt"  # srt file directory
+writedirectory = "./subtxt/"  # text file directory
+for filename in glob.glob(os.path.join(readdirectory, "*.srt")):
     subs = pysrt.open(filename)
     filename = Path(filename).stem
-    subtext = open(writedirectory + str(filename) + ".txt","w+")
+    subtext = open(writedirectory + str(filename) + ".txt", "w+")
     for sub in subs:
-        char_str = '' .join((z for z in sub.text if not z.isdigit()))
+        char_str = "".join((z for z in sub.text if not z.isdigit()))
         subtext.write(char_str)
-    subtext.close()   
+    subtext.close()
